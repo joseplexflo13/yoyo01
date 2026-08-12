26070760b71a5a245c43bfd2661f7e780efb3daa
Josep Lezma Florida <joseplezma@gmail.com>
2026-08-11 20:44:58 -0500

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-// ============================================================
-//  CONFIGURACIÓN
-//  Pega aquí la URL de tu implementación de Google Apps Script
-//  (Implementar > Nueva implementación > Aplicación web)
-// ============================================================
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwmJlDRumieqvGoRwR3Dsdl0qlfBPR3aoPpHhfFX-bZy4VAew9_S0RcMyJXOIYwuj0T/exec';
-
-// Tipos de tarea disponibles (deben coincidir con las pestañas)
-const TASK_TYPES = ['Personal', 'Educación', 'Trabajo'];
